c8b25e6582fd5a9e3fea5dde7659ae37151f1828
wqshen <wqshen91@gmail.com>
2016-06-10 15:37:50 +0800

    test

--- conf.py
+++ conf.py
@@ -110,6 +110,9 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 
+# Options for extensions.
+disqus_shortname = 'wqshen'
+
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
