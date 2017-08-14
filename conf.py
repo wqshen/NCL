3b00e632ff1e6caafb93f804e8af058f6f81d5b6
wqshen <wqshen91@gmail.com>
2017-08-14 16:58:38 +0800

    remove rst2pdf.pdfbuilder
    Fix the "Could not import extension rst2pdf.pdfbuilder" error in
    readthedocs

--- conf.py
+++ conf.py
@@ -32,7 +32,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax', 'rst2pdf.pdfbuilder', 'sphinxcontrib.disqus']
+
+if on_rtd:
+	extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.disqus']
+else:
+	extensions = ['sphinx.ext.mathjax', 'rst2pdf.pdfbuilder', 'sphinxcontrib.disqus']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
